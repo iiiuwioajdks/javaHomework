3c30e4beb53eed43ebbb210497181933e5c68e9c
Kas14 <1150881814@qq.com>
2021-06-18 02:51:51 +0800

    小修小补

--- src/FileManager/Action/AllAction.java
+++ src/FileManager/Action/AllAction.java
@@ -107,7 +107,7 @@ public class AllAction implements ActionListener {
                     } catch (Exception E) {
                     }
                 }
-                
+
             }
         });
 
