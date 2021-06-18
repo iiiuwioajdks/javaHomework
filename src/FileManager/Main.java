f1e3ce4f58cbe2c401bd61ec0c42010ca357b0a6
Kas14 <1150881814@qq.com>
2021-06-19 00:35:10 +0800

    加入搜索框

--- src/FileManager/Main.java
+++ src/FileManager/Main.java
@@ -78,15 +78,16 @@ public class Main{
                 // 去除虚线框
                 b7.setFocusPainted(false);
 
+                JTextField textField = new JTextField(8);
+                textField.setFont(new Font(null, Font.PLAIN, 20));
+                textField.setBounds(640,60,350,30);
+                menuFrame.add(textField);
+
                 b1.doClick();
                 b6.doClick();
 
-                final JTextField textField = new JTextField(8);
-                textField.setFont(new Font(null, Font.PLAIN, 20));
-                menuFrame.add(textField);
 
-//                Button b8 = new Button("删除",new DeleteAction());
-//                b8.setBounds(1000,0,99,50);
+
                 menuFrame.add(b1);
                 b1.addMouseListener(new MouseAdapter() {
                     @Override
@@ -107,27 +108,7 @@ public class Main{
                 menuFrame.add(b6);
                 menuFrame.add(b7);
                 menuFrame.add(b8);
-                //显示布局
-//                String[] columnType = {"文件名"};
-//                String[] file_name = current.list();
-//                int length = file_name.length;
-//                Object[][] real_file_name = new Object[length][1];
-//                for(int i = 0; i < length; i++){
-//                    real_file_name[i][0] = file_name[i];
-//                }
-//                JTable show_info = new JTable(real_file_name, columnType);
-//                TableColumn column = null;
-//                int column_height = show_info.getColumnCount();
-//                for(int i = 0; i < column_height; i++)
-//                {
-//                    column = show_info.getColumnModel().getColumn(i);
-//                    column.setPreferredWidth(100+500*i);
-//                }
-//                show_info.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
-//                JScrollPane scroll = new JScrollPane(show_info);
-//                scroll.setBounds(300,200,800,500);
-//
-//                menuFrame.add(scroll);
+
                 menuFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
                 menuFrame.setVisible(true);
             }
